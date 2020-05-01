9d4741cef39ab756d269e126252ee227232943b4
jackaraz <jackaraz@gmail.com>
2020-05-01 20:20:51 +0100

    optimization

--- JobWriter.py
+++ JobWriter.py
@@ -7,7 +7,7 @@ Created on Thu Jan 30 17:13:27 2020
 @contact : Jack Y. Araz <jackaraz@gmail.com>
 """
 
-import os, logging
+import os, logging, subprocess
 from .JobControl import JobControl
 
 
@@ -54,6 +54,8 @@ class JobWriter:
             pass # check this part!!
         self.JobIDinit = 0
 
+
+
     def write(self,*args,**kwargs):
         if args == []:
             jobID = max([int(x.split('.sh')[0].split('_')[1]) \
@@ -85,30 +87,40 @@ class JobWriter:
         if len(self.source) != 0:
             file.write('\n'.join(self.source)+'\n\n')
 
-        run_command =  kwargs.get('command',[])
-        if type(run_command) == str:
-            run_command = [run_command]
-        run_command = self.core_command + run_command
+        self.run_command =  kwargs.get('command',[])
+        if type(self.run_command) == str:
+            self.run_command = [self.run_command]
+        run_command = self.core_command + self.run_command
         file.write(' '.join(run_command)+'\n')
         file.write('exit 0\n')
         file.close()
         return True
+
+
 
     def occupied_list(self):
         """
             Creates a list of the computers that are already have jobs.
             PS: if the server is overloaded, workstations will not be excluded.
         """
-        os.system('squeue > .log.txt')
-        with open('.log.txt','r') as f:
-            job_list = f.readlines()
-        job_list = job_list[1:len(job_list)]
+        job_list = subprocess.Popen('squeue', shell=True,
+                                     stdout=subprocess.PIPE,
+                                     stderr=subprocess.STDOUT)
+        job_list = job_list.stdout.read().decode('utf-8').split('\n')[1:-1]
+        # try:
+        #     job_list = subprocess.run(['squeue'], stdout=subprocess.PIPE)
+        #     job_list = job_list.stdout.decode('utf-8').split('\n')[1:-1]
+        # except:
+        #     job_list = subprocess.check_output(['squeue'])
+        #     job_list = job_list.split('\n')[1:-1]
+        # os.system('squeue > .log.txt')
+        # with open('.log.txt','r') as f:
+        #     job_list = f.readlines()
+        # job_list = job_list[1:len(job_list)]
         ls_temp = [x.split()[len(x.split())-1] for x in job_list if not '(' in x]
-        me = os.environ.get('LOGNAME',False)
-        if me != False:
-            myjobs = [x.split()[len(x.split())-1] for x in job_list if not '(' in x and me in x]
+        myjobs  = [x.split()[len(x.split())-1] for x in job_list if not '(' in x and os.environ['LOGNAME'] in x]
         ls = []
-        os.remove('.log.txt')
+        # os.remove('.log.txt')
         for i in ls_temp:
             if self.just_submit:
                 if not i in myjobs and not i in ls:
@@ -116,10 +128,9 @@ class JobWriter:
             else:
                 if not i in ls:
                     ls.append(i)
-        if len(ls) >= 20 and not self.just_submit:
-            if me != False:
-                return [x for x in ls if not 'ip3' in x and x not in myjobs]
-            return [x for x in ls if not 'ip3' in x]
+        if len(ls) >= 20 or self.just_submit:
+            return [x for x in ls if not 'ip3' in x and x not in myjobs]
+            #return [x for x in ls if not 'ip3' in x]
         return ls
 
 
@@ -145,28 +156,56 @@ class JobWriter:
             print('Can not find '+filename+'.sh')
 
 
-    def Submit(self,remove_after_submission=False, **params):
+    def Submit(self,remove_after_submission=False,_try=0, **params):
         filename = params.get('filename',self.filename)
         if filename.endswith('.sh'):
             filename = filename.split('.sh')[0]
-        os.system('sbatch '+filename+'.sh > .temp.txt')
-        with open('.temp.txt','r') as f:
-            temp = f.readlines()
-        if temp == []:
-            os.remove('.temp.txt')
-            print('Can not retreive job information for '+filename)
-            print('Please double check if its submitted properly: ')
-            os.system('squeue -u '+os.environ['LOGNAME'])
-        else:
-            temp = temp[0].split()[len(temp[0].split())-1]
+
+        jobID     = self._submit(filename)
+        check_job = self._checkJob(jobID)
+
+        if check_job == -1:
+            return
+        elif check_job == 0:
+            self.just_submit = True
+            if self.write(filename,command=self.run_command):
+                jobID     = self._submit(filename)
+                check_job = self._checkJob(jobID)
+
+        if check_job == 1:
             submit_log = open(self.submit_log,'a+')
-            submit_log.write(filename+' '+temp+'\n')
+            submit_log.write(filename+' '+jobID+'\n')
             submit_log.close()
-            os.remove('.temp.txt')
             if remove_after_submission:
                 os.remove(filename+'.sh')
                 self.JobIDinit += 1
+            return
 
+
+    def _checkJob(self,jobID):
+        cmd_check = 'squeue -u '+os.environ['LOGNAME']+' | grep '+jobID
+        check_job = subprocess.Popen(cmd_check, shell=True,
+                                     stdout=subprocess.PIPE,
+                                     stderr=subprocess.STDOUT)
+        check_job = check_job.stdout.read().decode('utf-8')
+        if check_job == '':
+            print(u'\u001b[31m   * '+self.filename+ ': not running...\u001b[0m')
+            return -1
+        elif 'ReqNodeNotAvail' in check_job:
+            print(u'\u001b[31m   * '+self.filename+ ': Cant find available node, cancelling...\u001b[0m')
+            os.system('scancel '+jobID)
+            return 0
+        else:
+            return 1
+
+
+    def _submit(self,filename):
+        cmd_submit = 'sbatch ' + filename+'.sh'
+        sbatch = subprocess.Popen(cmd_submit, shell=True, 
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.STDOUT)
+        sbatch = sbatch.stdout.read().decode('utf-8')
+        return sbatch.split()[-1]
 
 
 
